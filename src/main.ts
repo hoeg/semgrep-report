9a86079c58565d2ebed22c239b1103f256c9191d
Peter Hoeg Steffensen <peter.steffensen@gmail.com>
2022-11-12 08:51:17 +0000

    fix linting

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
+import * as comments from './comments'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import * as comments from './comments'
+import {promises as fs} from 'fs'
 
 async function run(): Promise<void> {
   try {
-    const report_path: string = core.getInput("report_path")
-    const issue_number: number = +core.getInput("issue_number")
-    const owner: string = core.getInput("owner")
-    const repo: string = core.getInput("repo")
+    const report_path: string = core.getInput('report_path')
+    const issue_number: number = +core.getInput('issue_number')
+    const owner: string = core.getInput('owner')
+    const repo: string = core.getInput('repo')
 
-    const secret = core.getInput('github_secret');
+    const secret = core.getInput('github_secret')
     core.debug(`Ready to read report semgrep from ${report_path}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     const octokit = github.getOctokit(secret)
-    
-    const { promises: fs } = require('fs');
-    let content = await fs.readFile(report_path, 'utf8');
-    let params = comments.parseParams(content);
 
-    for(let p of params){
+    const content = await fs.readFile(report_path)
+    const params = comments.parseParams(content)
+
+    for (const p of params) {
       octokit.rest.pulls.createReviewComment({
-        owner: owner,
-        repo: repo,
+        owner,
+        repo,
         pull_number: issue_number,
-        body: p["body"],
-        path: p["path"],
-        start_line: p["start_line"],
-        line: p["end_line"]
-      });
+        body: p['body'],
+        path: p['path'],
+        start_line: p['start_line'],
+        line: p['end_line']
+      })
     }
     //core.setOutput('time', new Date().toTimeString())
   } catch (error) {
