cf64b615832ade7d3f18e82530e3eb8f909669df
Peter Hoeg Steffensen <peter.steffensen@gmail.com>
2022-11-20 14:01:47 +0000

    removed unneeded inputs

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import {promises as fs} from 'fs'
 async function run(): Promise<void> {
   try {
     const report_path: string = core.getInput('report_path')
-    const issue_number: number = +core.getInput('issue_number')
-    const r: string = core.getInput('repo')
-    const commitID: string = core.getInput('commit_id')
+    const issue_number: number = +github.context.payload.event.number
+    const r: string = github.context.repo.repo
+    const base = github.context.payload.pull_request?.base?.sha
+    const head = github.context.payload.pull_request?.head?.sha
 
     const secret = core.getInput('github_secret')
     core.debug(`Ready to read report semgrep from ${report_path}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
@@ -18,15 +19,11 @@ async function run(): Promise<void> {
     const content = await fs.readFile(report_path, 'utf-8')
     const params = comments.parseParams(content)
 
-    //lookup files changed
-    const base = github.context.payload.pull_request?.base?.sha
-    const head = github.context.payload.pull_request?.head?.sha
-
     const response = await octokit.rest.repos.compareCommits({
       base,
       head,
       owner: github.context.repo.owner,
-      repo: github.context.repo.repo
+      repo: r
     })
 
     // Ensure that the request was successful.
@@ -62,13 +59,13 @@ async function run(): Promise<void> {
         const owner: string = repository[0]
         const repo: string = repository[1]
         core.debug(
-          `create comment with: ${owner}, ${repo}, ${issue_number}, (${commitID}) ${p['body']}, ${p['path']} ${p['start_line']} ${p['end_line']}`
+          `create comment with: ${owner}, ${repo}, ${issue_number}, (${head}) ${p['body']}, ${p['path']} ${p['start_line']} ${p['end_line']}`
         )
         await octokit.rest.pulls.createReviewComment({
           owner,
           repo,
           pull_number: issue_number,
-          commit_id: commitID,
+          commit_id: head,
           body: p['body'],
           path: p['path'],
           start_line: p['start_line'],
