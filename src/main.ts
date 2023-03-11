0f2aa66a8de93be789d16b52def540ef0222f095
Peter Hoeg Steffensen <peter.steffensen@gmail.com>
2023-03-11 16:11:00 +0000

    test exist error

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,7 @@ async function run(): Promise<void> {
     const octokit = github.getOctokit(secret)
 
     if (!fileExists(report_path)) {
-      core.info(`${report_path} does not exist. Stopping action.`)
-      return
+      core.setFailed(`${report_path} does not exist. Stopping action.`)
     }
     const content = await fs.readFile(report_path, 'utf-8')
     const params = comments.parseParams(content)
