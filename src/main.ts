05f7f29ecf8622e272887a8a5e24763304e9fdb2
Peter Hoeg Steffensen <peter.steffensen@gmail.com>
2023-06-18 15:14:10 +0000

    use names instead of sha

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function run(): Promise<void> {
     const report_path: string = core.getInput('report_path')
     const issue_number: number = github.context.issue.number
     const r: string = github.context.repo.repo
-    const base = github.context.payload.pull_request?.base?.sha
-    const head = github.context.payload.pull_request?.head?.sha
+    const base = github.context.payload.pull_request?.base?.ref
+    const head = github.context.payload.pull_request?.head?.ref
 
     const secret = core.getInput('github_secret')
     const octokit = github.getOctokit(secret)
@@ -26,7 +26,7 @@ async function run(): Promise<void> {
     const response = await octokit.rest.repos.compareCommitsWithBasehead({
       owner: github.context.repo.owner,
       repo: r,
-      basehead: `${base}...${head}`
+      basehead: `main...${head}`
     })
 
     // Ensure that the request was successful.
